be3f4796bd3c4d315161d6e4db1289d065bdef13
Rohan <rohankhopade001@gmail.com>
2023-01-18 11:01:54 +0530

    Added Test Class

---
+++ src/com/velocity/project/Test.java
@@ -0,0 +1,17 @@
+package com.velocity.project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("Java");
+		list.add("Hibernate");
+		System.out.println(list);
+		
+			
+	}
+
+}
